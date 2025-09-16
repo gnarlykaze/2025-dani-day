0dc4e6a117f6cc07aed32a55b4fddbd3888e49b5
gnarlykaze <kyoberries17@gmail.com>
2025-09-16 17:14:54 +0530

    Initial commit

---
+++ api/unlock.js
@@ -0,0 +1,12 @@
+export default function handler(req, res) {
+  const { password } = req.query;
+
+  // 🔑 Set your secret password here
+  const correctPassword = "manon2025";
+
+  if (password === correctPassword) {
+    res.status(200).json({ success: true });
+  } else {
+    res.status(200).json({ success: false });
+  }
+}
